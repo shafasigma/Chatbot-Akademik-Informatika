91b6c32c45203aa0dff3fda790bc76a129ce54ef
shafasigma <wibisonoshafa2@gmail.com>
2025-11-25 14:44:29 +0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,13 +2,14 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 if DATABASE_URL:
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 else:
+    # fallback jika lokal
     DB_USER = "root"
     DB_PASS = ""
     DB_HOST = "localhost"
@@ -16,12 +17,14 @@ else:
     app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SECRET_KEY"] = "change_this_secret"
+app.config["SECRET_KEY"] = "supersecret"
 
 db = SQLAlchemy(app)
+
 MODEL_DIR = os.path.join(BASE_DIR, "model")
 UPLOADS_DIR = os.path.join(BASE_DIR, "uploads")
 os.makedirs(MODEL_DIR, exist_ok=True)
 os.makedirs(os.path.join(UPLOADS_DIR, "datasets"), exist_ok=True)
 os.makedirs(os.path.join(UPLOADS_DIR, "pdfs"), exist_ok=True)
+
 
